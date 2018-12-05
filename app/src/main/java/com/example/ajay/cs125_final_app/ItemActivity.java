8e1c798f80540fa719e3513df4ce600c26b82356
Ajay MT <ajay.tatachar@gmail.com>
2018-12-05 11:39:05 -0600

    calendar and location button click handlers

--- app/src/main/java/com/example/ajay/cs125_final_app/ItemActivity.java
+++ app/src/main/java/com/example/ajay/cs125_final_app/ItemActivity.java
@@ -6,11 +6,12 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -19,17 +20,20 @@ public class ItemActivity extends AppCompatActivity {
     private EditText itemName;
     private EditText itemDate;
     private Item item;
+    private Button calendarButton;
+    private Button locationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
+        final ItemActivity delegate = this;
+
         setTitle("Edit Item...");
 
         itemDate = findViewById(R.id.item_date);
         itemDate.setShowSoftInputOnFocus(false);
-        final ItemActivity delegate = this;
         itemDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -40,6 +44,22 @@ public class ItemActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 delegate.pickDate();
+            }
+        });
+
+        calendarButton = findViewById(R.id.calendar_button);
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(delegate, "Check calendar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        locationButton = findViewById(R.id.location_button);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(delegate, "Set location", Toast.LENGTH_SHORT).show();
             }
         });
     }
